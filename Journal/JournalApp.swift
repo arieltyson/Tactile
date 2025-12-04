0e6aa639745ff347cd840cf6e9d4cc3fb936147f
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-12-04 00:26:55 -0800

    Initial commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Ariel Tyson on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
